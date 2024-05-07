54b445290e1cf5ca35d069a7715558998bc9d76b
Aish Mishra <aishlunatic001@gmail.com>
2024-05-07 14:38:00 +0530

    created route structing

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,10 +1,12 @@
 
 
 const express = require('express') ; 
+const userRouter = require('./user');
 const app = express() ; 
 
 const route = express.Router() ;  
 
+route.use("/user",userRouter)
 module.exports = {
   route 
 }
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,7 @@
-const express = require("express");
+const express = require("express")
 
-const bodyParser = require("body-parser") ; 
+const mainRouter = require('../backend/Routes/index');
 
-const app = express(); 
+app.use("api/v1",mainRouter) ; 
 
-app.get('/signup',(req,res)=>{
-   const username = req.body.username;
-   const password = req.body.password;
-
-app.Schema(  )
-
-
-})
-
+//  By calling app.use("api/v1", mainRouter), the mainRouter middleware function is mounted at the /api/v1 path, and any requests that match this path will be handled by mainRouter.
