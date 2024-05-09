f7b41ba366413ada4b39fda87af9a54223266f17
Aish Mishra <aishlunatic001@gmail.com>
2024-05-09 15:06:56 +0530

    signin and signUp route create with mongoDb connection

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -4,17 +4,20 @@ const zod = require('zod') ;
 const User = require('../db') ; 
 const { JWT_SECRET } = require('../config');
 
+// Signup Schema : 
 
-const userSchema = zod.object({
+const signUpSchema = zod.object({
   username: zod.string().email(),
   firstname: zod.string(),
   lastname: zod.string(),
   password: zod.string()
 });
 
+// signin Route : 
+
 userRouter.post('/signup', async (req, res) => {
   
-  const validData = userSchema.safeParse(req.body);
+  const validData = signUpSchema.safeParse(req.body);
 
     if (!validData.success) {
       res.status(404).json({ message: "Email already taken / Incorrect inputs" });
@@ -50,5 +53,45 @@ res.status(201).json({
 })
 
 });
+
+
+// signup Schema : 
+
+
+
+const signInSchema = zod.object({
+   username : zod.string() ,
+   password : zod.string().number() 
+})
+
+// sign
+
+userRouter.post("/signin", async (req,res)=>{
+  
+
+
+  const validUser = signInSchema.bodyParser(req.body) ; 
+
+  if(!validUser.success){
+    res.status(400).json({
+      message: "Error while logging in"
+    })
+  }
+
+  const user = await User.findOne({
+    username : req.body.username ,
+    password : req.body.password 
+  }) ; 
+  if(!user){
+    res.status(411).json({ message: "Error while logging in"});
+  } 
+
+  const token = jwt.sign({
+    token 
+  }, JWT_SECRET) ; 
+   res.json({
+    token : token 
+   })
+})
 
 module.exports = userRouter;
