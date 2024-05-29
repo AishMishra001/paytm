5d4efd276b3bffe285163335284e47c7429f7dcf
Aish Mishra <aishlunatic001@gmail.com>
2024-05-29 21:15:52 +0530

    UserRouter signin and signup page

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,98 +1,101 @@
-const express = require('express');
-const userRouter = express.Router();
-const zod = require('zod') ; 
-const User = require('../db') ; 
+const express = require('express') ; 
+const UserRouter = express.Router() ;
+const zod = require('zod');  
+const { User } = require('../db');
 const { JWT_SECRET } = require('../config');
+const jwt = require('jsonwebtoken') ; 
+const { read } = require('graceful-fs');
 
-// Signup Schema : 
 
-const signUpSchema = zod.object({
-  username: zod.string().email(),
-  firstname: zod.string(),
-  lastname: zod.string(),
-  password: zod.string()
-});
+// Creating schema 
 
-// signin Route : 
+signUpSchema = mongoose.Schema({
+  username: zod.string().email() , 
+  password: zod.string(), 
+  firstname : zod.string() , 
+  lastname : zod.string() ,
+})
+UserRouter.post('/signup', async (req,res)=>{
 
-userRouter.post('/signup', async (req, res) => {
-  
-  const validData = signUpSchema.safeParse(req.body);
+  // checking validity with safeParse 
+    const ValidData = signUpSchema.safeParse(req.body) ; 
 
-    if (!validData.success) {
-      res.status(404).json({ message: "Email already taken / Incorrect inputs" });
-    } 
-    res.status(200).json({
-      message: "User created successfully",
-      token: "jwt"
-    });
-  
-    const existingUser = User.findOne({
-      username : req.body.username 
-    })
-
-    if(existingUser){
-      res.status(404).json({ message: "Email already taken/Incorrect inputs"});
+  // if Input data is not valid send a status code of 400 and message of incorrect user 
+    
+    if(!ValidData.success){
+      res.status(400).json({
+        message : 'Incorrect inputs'
+      }) ;
     }
 
-    const user = User.create({
-      username : req.body.username , 
-      firstname : req.body.firstname , 
-      lastname : req.body.lastname , 
-      password : req.body.password , 
+    // After checking the validity ..... if the data is valid check if the user already existing the database 
+
+    const excitingUser = await User.findOne({
+      username : body.username  
     })
 
-const userId = user._id  ; 
+   
 
-const token = jwt.sign({
-  userId
-},JWT_SECRET)
+    if(excitingUser){
+      res.status(400).json({
+        message : 'User already exists'
+      })
+    }
 
-res.json({
-  message: "User created successfully",
-  token: token ,
+    // Now create the new user in the database 
+
+       const user = await User.create({
+          username : req.body.username , 
+          firstname : req.body.firstname ,
+          lastname : req.body.lastname , 
+          password : req.body.password 
+       }) 
+
+       const userId = user._id ; 
+
+
+       // response with a new jwt token after creating a new user 
+
+       const token = jwt.sign({
+         userId 
+        }, JWT_SECRET) ; 
+
+        res.json({
+          message : "User successfully created " , 
+          token : token 
+        })
 })
 
-});
-
-
-// signup Schema : 
-
-
-
-const signInSchema = zod.object({
-   username : zod.string() ,
-   password : zod.string().number() 
+const signUpSchema = mongoose.Schema({
+  username : zod.string(),
+  password : zod.string(),
 })
+UserRouter.post('/signin', async (req,res)=>{
+      const validUser = signUpSchema.safeParse(req.body) ; 
 
-// sign
+      if(!validUser.success){
+        res.status(411).json({
+          message : 'Incorrect Inputs'
+        })
+      }
 
-userRouter.post("/signin", async (req,res)=>{
-  
+      const existingUser = User.findOne({
+        username : req.body.username , 
+        password : req.body.password 
+      })
 
+      if(existingUser){
+        
+          const token = jwt.sign({
+            userId : existingUser._id 
+          }, JWT_SECRET ) ; 
+        
+          res.json({
+             token : token 
+          })
+      }
 
-  const validUser = signInSchema.bodyParser(req.body) ; 
-
-  if(!validUser.success){
-    res.status(400).json({
-      message: "Error while logging in"
-    })
-  }
-
-  const user = await User.findOne({
-    username : req.body.username ,
-    password : req.body.password 
-  }) ; 
-  if(!user){
-    res.status(411).json({ message: "Error while logging in"});
-  } 
-
-  const token = jwt.sign({
-    token 
-  }, JWT_SECRET) ; 
-   res.json({
-    token : token 
-   })
+      res.status(411).json({
+        message : 'Error while logging in'
+      })
 })
-
-module.exports = userRouter;
