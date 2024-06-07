e06ff42b4ecb8f3478bdae62ccac19a54c066222
Aish Mishra <aishlunatic001@gmail.com>
2024-06-07 10:20:19 +0530

    added new icon

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { GiWallet } from "react-icons/gi";
+import { AiFillFire } from "react-icons/ai";
 
 function Appbar({ user, userImage }) {
   const displayUser = user || 'User';
 
   return (
     <div className='flex py-4 px-8 justify-between shadow items-center'>
-      <div className='text-black font-extrabold text-3xl flex gap-2'><GiWallet />EasyPay</div>
+      <div className='text-black font-extrabold text-3xl flex gap-2'><AiFillFire />EasyPay</div>
       <div className='flex gap-4 items-center'>
         <div className='text-black text-xl font-bold'>Hello, {displayUser}</div>
         <button className='bg-gray-300 rounded-full py-2 px-4'>{displayUser[0]?.toUpperCase()}</button>
